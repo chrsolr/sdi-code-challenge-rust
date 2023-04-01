172aa497868d4ca23708c4928e1f94fef1214e2a
chrsolr <chr.solr@gmail.com>
2023-04-01 00:45:17 -0500

    Getting CLI args and reading file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
+    // Get CLI arguments
     let args: Vec<String> = env::args().collect();
 
-    // Get args value
-    // -file [VALUE]
-    // -sort [VALUE]
-    dbg!(args);
+    // Get argument value
+    let file_value_index = args.iter().position(|v| v == "-file").unwrap() + 1;
+    let sort_value_index = args.iter().position(|v| v == "-sort").unwrap() + 1;
+    let file_value = &args[file_value_index];
+    let sort_value = &args[sort_value_index];
+
+    // Read File
+    let content = fs::read_to_string(file_value).expect("Expected to read the file");
+    
+    println!("{content}")
 }
+
+// https://doc.rust-lang.org/book/ch12-02-reading-a-file.html
+// cargo run -- -file ./src/data/medicalreports.txt -sort PatientID

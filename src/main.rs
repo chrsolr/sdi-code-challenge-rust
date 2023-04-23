30399147272fc8fe9cd98ac1e97883a1454d364c
chrsolr <chr.solr@gmail.com>
2023-04-23 11:51:23 -0500

    Separated env parsing

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,30 @@ use std::{
     env,
     fs::{self, File},
     io::{self, BufRead},
-    path::Path,
+    path::Path, 
+    fmt::{self, Formatter, Display}
 };
+use text_colorizer::*;
+
+struct Arguments {
+    filename: String,
+    sort_by: String,
+}
 
 fn main() {
-    // Get CLI arguments
-    let args: Vec<String> = env::args().collect();
-
-    // Get argument value
-    let file_value_index = args.iter().position(|v| v == "-file").unwrap() + 1;
-    let sort_value_index = args.iter().position(|v| v == "-sort").unwrap() + 1;
-    let filename = &args[file_value_index];
-    let _sort_value = &args[sort_value_index];
+    let arguments = parse_args();
 
     // Read File
     // let content = fs::read_to_string(file_value).expect("Expected to read the file");
 
     // let a = read_file_lines(file_value);
-    split_records(filename);
+    split_records(arguments.filename);
 }
 
 // https://doc.rust-lang.org/book/ch12-02-reading-a-file.html
 // cargo run -- -file ./src/data/medicalreports.txt -sort PatientID
 
-fn split_records(filename: &String) {
+fn split_records(filename: String) {
     if let Ok(lines) = read_file_lines(filename) {
         for line in lines {
             if let Ok(ip) = line {
@@ -51,4 +51,36 @@ where
 {
     let file = fs::File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+
+fn parse_args() -> Arguments {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() != 4 {
+        print_how_to_use();
+        eprintln!("{} wrong number of arguments: expect 4, got {}", "Error".red().bold(), args.len());
+        std::process::exit(1);
+    }
+
+    let file_value_index = args.iter().position(|v| v == "--file").unwrap() + 2;
+    let sort_value_index = args.iter().position(|v| v == "--sort").unwrap() + 2;
+
+    let filename = env::args().nth(file_value_index).expect("missing file: --file");
+    let sort_by = env::args().nth(sort_value_index).expect("missin sort: --sort");
+
+
+    Arguments { filename, sort_by }
+}
+
+fn print_how_to_use() -> () {
+    eprintln!(
+        "{} parse a file and sort by a given field",
+        "SDI Challenge:".blue()
+    );
+    eprintln!(
+        "Usage: {} {}",
+        "SDI Challenge:".blue(),
+        "--file <file location> --sort <sort field>".green()
+    );
 }
